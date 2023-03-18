063199e04fc244817c7a97293785741dd29ba34d
serdarferhat <cylnsrdrr@gmail.com>
2023-03-18 11:16:29 +0300

    useRef_value-tutarken

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Auth from "./components/Auth";
+import Chat from "./components/Chat";
+import "./styles/style.css";
 
 function App() {
-  const [isAuth,setIsAuth]=useState(localStorage.getItem("token"))
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("token"));
+  const inputRef = useRef(null);
+  const [room, setRoom] = useState(null);
 
-  if(!isAuth){
-    <Auth/>
+  if (!isAuth) {
+    <Auth />;
   }
   return (
-    <div className="App">
-      Oda İsmi Giriniz
+    <div className="container">
+      {room ? (
+        <Chat />
+      ) : (
+        <div className="container-room">
+          {" "}
+          <h1>Sohbet Odası</h1>
+          <p>Hangi Odaya Gireceksiniz?</p>
+          <input type={"text"} ref={inputRef}></input>
+          <button
+            onClick={() => {
+              setRoom(inputRef.current.value);
+            }}
+          >
+            Odaya Gir
+          </button>
+        </div>
+      )}
     </div>
   );
 }
